095dd4c60a01177716b4e93be655f3211b0569f2
Babicheva <ababiche@t-systems.ru>
2021-08-07 02:10:51 +0300

    frontend edit

--- backend/app.js
+++ backend/app.js
@@ -61,6 +61,11 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().custom(isEmail),
@@ -84,6 +89,4 @@ app.use('*', (req, res, next) => {
 });
 app.use(error);
 
-app.listen(PORT, () => {
-  console.log('app listening port', PORT);
-});
+app.listen();
